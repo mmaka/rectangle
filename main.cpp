b3e09a37694662baad0b0dfec83123d4964fe77d
Michał Makaś <michalmakas@gmail.com>
2019-11-07 23:36:09 +0100

    test

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
 
-using namespace nana;
-
 int main()
 {
-  form fm;
   [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n";}();
 }
