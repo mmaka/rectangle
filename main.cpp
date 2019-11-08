1fa04656f1ed12b3edd3613e38652f2f90c24aeb
maka <michalmakas@gmail.com>
2019-11-08 19:37:34 +0100

    asd2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<functional>
-#include <nana/gui.hpp>
-#include <nana/gui/widgets/label.hpp>
-#include <nana/gui/widgets/button.hpp>
+//#include <nana/gui.hpp>
+//#include <nana/gui/widgets/label.hpp>
+//#include <nana/gui/widgets/button.hpp>
 
 int main()
 {
--- tester.cpp
+++ tester.cpp
@@ -1,3 +1,6 @@
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
 int main()
 {
   return 0;
